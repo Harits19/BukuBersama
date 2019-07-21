735e9171c9ad8b996137660da22f75046b5216bd
Harits19 <38558796+Harits19@users.noreply.github.com>
2019-07-22 00:57:22 +0700

    PopUp pembayaran

--- app/src/main/java/com/example/bukubersama/PinjamBukuActivity.java
+++ app/src/main/java/com/example/bukubersama/PinjamBukuActivity.java
@@ -1,20 +1,26 @@
 package com.example.bukubersama;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 public class PinjamBukuActivity extends AppCompatActivity {
 
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pinjam_buku);
+        myDialog = new Dialog(this);
 
 
 
@@ -26,4 +32,19 @@ public class PinjamBukuActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Novel"));
         tabLayout.addTab(tabLayout.newTab().setText("Tafsir"));
     }
+
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        myDialog.setContentView(R.layout.popup_detail_pembayaran_peminjaman);
+        txtclose =(TextView) myDialog.findViewById(R.id.closeTextView);
+        txtclose.setText("M");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
 }
+    }
